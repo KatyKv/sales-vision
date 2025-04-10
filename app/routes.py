bca5fc1dc37ee32a8700147e82a72484687fa30a
Alexander Sender <alexander.sender@gmail.com>
2025-04-10 08:39:40 +0300

    share data on web

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,43 @@
-from flask import Blueprint, render_template, request, jsonify, send_from_directory, current_app
+from datetime import datetime
+from flask import Blueprint, render_template, request, jsonify, send_from_directory, current_app, send_file, session
 from .data_loader import process_csv
 from flask import redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import RegistrationForm, LoginForm, EditForm
 from app import db, bcrypt
 from app.models import User
+import os
+#from .analytics import generate_analysis
+from .visualization import plot_sales_trend, plot_top_products, plot_sales_by_region
+import pandas as pd
+import xlsxwriter
+import logging
 
+
+######
+from .analytics import load_data, calculate_metrics, sales_by_date, sales_by_month, top_products, sales_by_region
+
+######
 
 # Создаем Blueprint вместо прямого использования app
 main_bp = Blueprint("main", __name__)
 
-# @main_bp.route('/')
-# def index():
-#     return render_template('index.html')
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    handlers=[
+        logging.FileHandler("app.log"),   # лог в файл
+        logging.StreamHandler()           # лог в консоль
+    ]
+)
+
+#############################
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
+RESULT_FOLDER = os.path.join(BASE_DIR, 'results')
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(RESULT_FOLDER, exist_ok=True)
+###############################
 
 @main_bp.route('/upload', methods=['POST'])
 def upload():
@@ -21,11 +46,12 @@ def upload():
 
     file = request.files['file']
     result = process_csv(file, current_app.config['UPLOAD_FOLDER'])  # Используем current_app
+
+    if result.get('status') == 'success':
+        session['saved_filename'] = result['saved_as']
     return jsonify(result)
 
-@main_bp.route('/download/<filename>')
-def download(filename):
-    return send_from_directory(current_app.config['UPLOAD_FOLDER'], filename)
+
 
 @main_bp.route("/")
 def home():
@@ -85,16 +111,58 @@ def edit():
 
 @main_bp.route('/logout')
 def logout():
-    logout_user()  # что делает эта команда??
+    logout_user()
     return redirect(url_for('main.home'))
 
 @main_bp.route('/account')
-@login_required  # для чего это здесь нужно???
+@login_required
 def account():
-    form = LoginForm()
-    return render_template('account.html', form=form, title='Account')
+    return render_template('account.html', username=current_user.username)
 
 
 @main_bp.route('/load_csv', methods=['GET', 'POST'])
 def load_csv():
-        return render_template('load_csv.html')
+    return render_template('load_csv.html')
+
+#############################
+@main_bp.route('/generate_report', methods=['POST'])
+def generate_report():
+
+    filename = session.get('saved_filename')
+    if not filename:
+        return "Файл не найден. Сначала загрузите CSV.", 400
+
+    data_file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+    df = load_data(data_file_path)
+
+    metrics = calculate_metrics(df)
+
+    df_by_date = sales_by_date(df)
+    df_by_month = sales_by_month(df)
+    df_by_region = sales_by_region(df)
+    df_top = top_products(df)
+
+    graphs = {
+        "Выручка по месяцам": plot_sales_trend(df_by_month),
+        "Топ продуктов": plot_top_products(df_top),
+        "Топ продуктов по количеству": plot_top_products(df_top, 'quantity'),
+        "Выручка по регионам": plot_sales_by_region(df_by_region)
+    }
+    return render_template('load_csv.html', graphs=graphs, metrics=metrics)
+
+@main_bp.route('/download_report')
+def download_report():
+    try:
+        filename = request.args.get('filename')
+        if not filename:
+            return "Не указано имя файла", 400
+
+        file_path = os.path.join(RESULT_FOLDER, filename)
+        if not os.path.exists(file_path):
+            return "Файл отчета не найден", 404
+
+        return send_file(file_path, as_attachment=True)
+
+    except Exception as e:
+        return f"Ошибка скачивания: {str(e)}", 500
+
